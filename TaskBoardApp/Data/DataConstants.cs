2cddeec6a496fa82e419558cd66c5b043b0ac58e
nedrett <nedret88@gmail.com>
2022-10-16 07:32:38 +0300

    Modified Register page to custom view

--- TaskBoardApp/Data/DataConstants.cs
+++ TaskBoardApp/Data/DataConstants.cs
@@ -6,6 +6,7 @@
         {
             public const int MaxUserFirstName = 15;
             public const int MaxUserLastName = 15;
+            public const int MaxUserUsername = 30;
         }
 
         public class Task
