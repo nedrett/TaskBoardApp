adba6d8d69c729543208f5383e8722805ca29efc
nedrett <nedret88@gmail.com>
2022-10-16 18:36:42 +0300

    Extended Home Page

--- TaskBoardApp/Controllers/HomeController.cs
+++ TaskBoardApp/Controllers/HomeController.cs
@@ -4,18 +4,49 @@ using TaskBoardApp.Models;
 
 namespace TaskBoardApp.Controllers
 {
+    using System.Security.Claims;
+    using Data;
+
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly TaskBoardAppDbContext data;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        public HomeController(TaskBoardAppDbContext context) => this.data = context;
 
         public IActionResult Index()
         {
-            return View();
+            var taskBoards = this.data
+                .Boards
+                .Select(b => b.Name)
+                .Distinct();
+
+            var tasksCounts = new List<HomeBoardModel>();
+            foreach (var boardName in taskBoards)
+            {
+                var tasksInBoard = this.data.Tasks.Where(t => t.Board.Name == boardName).Count();
+                tasksCounts.Add(new HomeBoardModel()
+                {
+                    BoardName = boardName,
+                    TasksCount = tasksInBoard
+                });
+            }
+
+            var userTasksCount = -1;
+
+            if (this.User.Identity.IsAuthenticated)
+            {
+                var currentUserId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+                userTasksCount = this.data.Tasks.Where(t => t.OwnerId == currentUserId).Count();
+            }
+
+            var homeModel = new HomeViewModel()
+            {
+                AllTasksCount = this.data.Tasks.Count(),
+                BoardsWithTasksCount = tasksCounts,
+                UserTasksCount = userTasksCount
+            };
+
+            return View(homeModel);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
