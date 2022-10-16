2d3af61a7d41f2a8a44c2b148bc97366eef8eb3e
nedrett <nedret88@gmail.com>
2022-10-16 07:36:56 +0300

    Modified Logout page

--- TaskBoardApp/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ TaskBoardApp/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -2,13 +2,9 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using System;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 using TaskBoardApp.Data.Entities;
 
 namespace TaskBoardApp.Areas.Identity.Pages.Account
@@ -16,18 +12,15 @@ namespace TaskBoardApp.Areas.Identity.Pages.Account
     public class LogoutModel : PageModel
     {
         private readonly SignInManager<User> _signInManager;
-        private readonly ILogger<LogoutModel> _logger;
 
-        public LogoutModel(SignInManager<User> signInManager, ILogger<LogoutModel> logger)
+        public LogoutModel(SignInManager<User> signInManager)
         {
             _signInManager = signInManager;
-            _logger = logger;
         }
 
         public async Task<IActionResult> OnPost(string returnUrl = null)
         {
             await _signInManager.SignOutAsync();
-            _logger.LogInformation("User logged out.");
             if (returnUrl != null)
             {
                 return LocalRedirect(returnUrl);
