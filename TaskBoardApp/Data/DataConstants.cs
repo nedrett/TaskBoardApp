ebe18642236b12b5fe55219803c1fb8308be6666
nedrett <nedret88@gmail.com>
2022-10-16 06:02:08 +0300

    Implemented Task Entity

--- TaskBoardApp/Data/DataConstants.cs
+++ TaskBoardApp/Data/DataConstants.cs
@@ -7,5 +7,14 @@
             public const int MaxUserFirstName = 15;
             public const int MaxUserLastName = 15;
         }
+
+        public class Task
+        {
+            public const int MaxTaskTitle = 70;
+            public const int MinTaskTitle = 5;
+
+            public const int MaxTaskDescription= 1000;
+            public const int MinTaskDescription = 10;
+        }
     }
 }
---
+++ TaskBoardApp/Data/Entities/Task.cs
@@ -0,0 +1,29 @@
+﻿namespace TaskBoardApp.Data.Entities
+{
+    using System.ComponentModel.DataAnnotations;
+    using static TaskBoardApp.Data.DataConstants.Task;
+    
+    public class Task
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(MaxTaskTitle)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(MaxTaskDescription)]
+        public string Description { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public int BoardId { get; set; }
+
+        public Board Board { get; set; }
+
+        [Required]
+        public string OwnerId { get; set; }
+
+        public User User { get; set; }
+    }
+}
